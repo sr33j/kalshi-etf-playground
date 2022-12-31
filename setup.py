c0a36289a30371fe344aeb6d5fe18e9b2677af78
Srijith Poduval <srijithpoduval@Srijiths-MBP-3.fios-router.home>
2022-12-31 16:41:48 -0500

    fixes

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ import os
 from engine import download_all_kalshi_market_info
 
 def main():
+    print("making missing folders and files...")
     ## if cache folder doesn't exist, create it
     if not os.path.exists("cache"):
         os.mkdir("cache")
+
+    ## if price_data folder doesn't exist in cache, create it
+    if not os.path.exists("cache/price_data"):
+        os.mkdir("cache/price_data")
     
     ## if etfs_created.csv doesn't exist, create it
     if not os.path.exists("cache/etfs_created.csv"):
@@ -13,6 +18,7 @@ def main():
     
     ## if full_market_data.csv doesn't exist, create it
     if not os.path.exists("cache/full_market_data.csv"):
+        print("downloading all kalshi market info...")
         download_all_kalshi_market_info()
         
 if __name__ == '__main__':
